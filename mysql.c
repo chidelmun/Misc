8324b64264544e3b6f965eb3b3ab085e7cb26d22
Delveri Munang <chickdelveri@gmail.com>
2016-07-15 10:15:07 +0100

    Created Mysql client

---
+++ mysql.c
@@ -0,0 +1,39 @@
+#include <my_global.h>
+#include <mysql.h>
+#include <string.h>
+
+#define QUERYLENGTH 100
+
+
+int main(int argc, char **argv)
+{  
+  MYSQL *con = mysql_init(NULL);
+  char query[QUERYLENGTH];
+
+  if (con == NULL) 
+  {
+      fprintf(stderr, "%s\n", mysql_error(con));
+      exit(1);
+  }
+
+  if (mysql_real_connect(con, "localhost", "root", "root", 
+          NULL, 0, NULL, 0) == NULL) 
+  {
+      fprintf(stderr, "%s\n", mysql_error(con));
+      mysql_close(con);
+      exit(1);
+  }  
+
+  while(fgets(query, QUERYLENGTH , stdin) != NULL)
+    {
+       if (mysql_query(con, query)) 
+        {
+          fprintf(stderr, "%s\n", mysql_error(con));
+          // mysql_close(con);
+          // exit(1);
+        }
+        printf("%s\n", query);
+    }
+  mysql_close(con);
+  exit(0);
+}
